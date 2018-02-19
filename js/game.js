f849f9b2940fe9028bec3b0c11694f8cd95c8178
Jen Lipton <jkl.lipton@gmail.com>
2018-02-19 15:10:13 -0800

    Ryan and Jen making game.js

--- js/game.js
+++ js/game.js
@@ -1,8 +1,45 @@
 const game = {
     player: '',
+    story: document.getElementById('story'),
 
     start: function (){
         this.player = JSON.parse(localStorage.getItem('settings'));
-        console.log(this.player);
+
+        if (this.player.choices.length === 0){
+            this.story.textContent = `Welcome, ${this.player.name}!`;
+        } else {
+            //load current position
+        }
+    },
+
+    reload: function (){
+        //delete previous text
+        switch (this.player.choices[this.player.choices.length - 1]){
+        case 'A':
+            console.log('You picked A!');
+            break;
+
+        case 'B':
+            console.log('You picked B!');
+            break;
+
+        default:
+            console.log('error!');
+            break;
+        }
+
     }
 };
+
+const choices = document.getElementById('choices');
+
+choices.a.addEventListener('click', function (){
+    game.player.choices.push('A');
+    game.reload();
+});
+
+choices.b.addEventListener('click', function (){
+    game.player.choices.push('B');    
+    game.reload();
+});
+
