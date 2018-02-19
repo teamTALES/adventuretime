51cca6b7e04eec502feeba66254a0c3d07e83796
Jen Lipton <jkl.lipton@gmail.com>
2018-02-19 13:28:06 -0800

    Working Intro page

--- js/app.js
+++ js/app.js
@@ -9,14 +9,33 @@ const Player = {
 
 
 const introForm = document.getElementById('character');
+const character1 = document.getElementById('character1');
+const character2 = document.getElementById('character2');
+
+character1.addEventListener('click', function(){
+    Player.character = 1;
+    character1.setAttribute('style', 'color: white;');
+    character2.setAttribute('style', 'color: black;');
+});
+
+character2.addEventListener('click', function(){
+    Player.character = 2;
+    character2.setAttribute('style', 'color: white;');
+    character1.setAttribute('style', 'color: black;');
+});
+
+
 console.log(introForm);
 introForm.addEventListener('submit', function(){
     event.preventDefault();
 
     Player.name = this['character'].value;
     console.log(Player.name);
+    console.log(Player.character);
+    window.location.href = 'game.html';
 
 });
+
 
 
 const Game = {
