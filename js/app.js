da55b1bfef0291af5d806edac15944f9ad46c721
Jen Lipton <jkl.lipton@gmail.com>
2018-02-19 13:17:28 -0800

    JS start

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+
+const Player = {
+    name: '',
+    character: 0,
+    choices: [],
+};
+
+
+const introForm = document.getElementById('character');
+console.log(introForm);
+introForm.addEventListener('submit', function(){
+    event.preventDefault();
+
+    Player.name = this['character'].value;
+    console.log(Player.name);
+
+});
+
+
+const Game = {
+    onLoad: function (){
+        //check for localStorage
+
+    }
+};
+
+
+
